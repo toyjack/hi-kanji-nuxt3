3474a8abf6ace353ca79dd7411bd55b3851c0ec3
toyjack <toyjack@gmail.com>
2022-06-02 23:24:00 +0900

    feat: press enter key to search

--- pages/search/[character].vue
+++ pages/search/[character].vue
@@ -49,7 +49,7 @@ function move() {
 <template>
   <div class="">
     <div class="flex flex-row p-1 md:py-10 md:px-20 items-center justify-center gap-1 md:gap-4">
-      <input type="text" placeholder="検索" v-model="inputCharacter"
+      <input type="text" placeholder="検索" v-model="inputCharacter" @keyup.enter.prevent="move"
         class=" border-transparent appearance-none border border-gray-300 py-2 px-4 w-2/3 md:w-1/5 bg-white rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
       <button type="button" @click="move"
         class="w-1/3 md:w-1/12 text-white bg-blue-500 rounded px-4 py-2 hover:bg-white hover:text-black duration-300">Search</button>
