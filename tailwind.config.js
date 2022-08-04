1a019659740ef0475f553b2e5cdfdcec38bdb581
toyjack <toyjack@gmail.com>
2022-08-04 13:16:09 +0900

    add: taliwind typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,24 @@ module.exports = {
     extend: {},
   },
   plugins: [
+    require("@tailwindcss/typography"),
     require("daisyui"),
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#1c3996",
+          "secondary": "#ea33a1",
+          "accent": "#98d3e2",
+          "neutral": "#382E38",
+          "base-100": "#F7F6F8",
+          "info": "#9EC3EA",
+          "success": "#137C62",
+          "warning": "#9F7A09",
+          "error": "#F64A37",
+        },
+      },
+    ],
+  },
 }
